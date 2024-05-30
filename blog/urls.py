a682048c876693071611368763ee62102d7d9ccb
Biringanine Destin <biringaninedestin@gmail.com>
2024-05-30 16:32:53 +0200

    add comments by post on urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PostList, PostDetail, CommentDetail, CommentList, HomeView
+from .views import PostList, PostDetail, CommentDetail, CommentList, HomeView, CommentByPost
 
 urlpatterns = [
     path('posts/', PostList.as_view()),
     path('post/<int:pk>/', PostDetail.as_view()),
     path('comments/', CommentList.as_view()),
     path('comment/<int:pk>/', CommentDetail.as_view()),
+    path('post/<int:pk>/comments/', CommentByPost.as_view()),
     path('', HomeView.as_view())
 ]
