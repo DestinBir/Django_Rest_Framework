128127521d72f06a3f4bc1a156026e6147340184
Biringanine Destin <biringaninedestin@gmail.com>
2024-05-30 14:43:54 +0200

    adding other method

--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,29 @@ class PostList(APIView):
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
+    
+    def post(self, request, format=None):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+    
+class PostDetail(APIView):
+    def get(self, request, pk, format=None):
+        post = Post.objects.get(pk=pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    
+    def put(self, request, pk, format=None):
+        post = Post.objects.get(pk=pk)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+    
+    def delete(self, request, pk, format=None):
+        post = Post.objects.get(pk=pk)
+        post.delete()
+        return Response(status=204)
