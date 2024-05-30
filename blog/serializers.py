884cbbac22ee348e10142ca8949006afab86e082
Biringanine Destin <biringaninedestin@gmail.com>
2024-05-30 15:30:02 +0200

    comment serializer

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Comment
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'title', 'content', 'pub_date']
+        
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'content', 'pub_date']
