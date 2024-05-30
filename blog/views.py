60c57233e1b7a1b2d91f979946eac2f4482ff29b
Biringanine Destin <biringaninedestin@gmail.com>
2024-05-30 14:13:47 +0200

    creating the post list view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+class PostList(APIView):
+    def get(self, request, format=None):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
