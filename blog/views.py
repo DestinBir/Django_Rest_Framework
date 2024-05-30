ef1e4a7137153d6732c7d74ef1ee21a00abc2326
Biringanine Destin <biringaninedestin@gmail.com>
2024-05-30 15:32:32 +0200

    comment views

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
-from .models import Post
-from .serializers import PostSerializer
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
@@ -14,6 +14,23 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def get_options(self, request, pk, format=None):
+        # Define allowed methods for this endpoint
+        return Response(allowed_methods=['GET', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'])
+
+
+class CommentList(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def get_options(self, request, format=None):
+        # Define allowed methods for this endpoint
+        return Response(allowed_methods=['GET', 'POST', 'OPTIONS'])
+    
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
 
     def get_options(self, request, pk, format=None):
         # Define allowed methods for this endpoint
