7441f5604084de6a6aa733e9693c62da82fbdb28
Biringanine Destin <biringaninedestin@gmail.com>
2024-05-30 15:28:02 +0200

    creating comment model

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,11 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
