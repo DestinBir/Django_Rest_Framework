0f15e9637d988e952bc110fb04285c1b7f69cea7
Biringanine Destin <biringaninedestin@gmail.com>
2024-05-30 17:21:38 +0200

    remove authentiction restrictions

--- restapi/settings.py
+++ restapi/settings.py
@@ -92,13 +92,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-    ],
+    
 }
 
 
